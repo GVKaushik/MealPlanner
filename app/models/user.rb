ace347dd0cc6fa7033988e4a7a013b6118b9a630
GVKaushik <gvkaushik11@gmail.com>
2025-05-14 09:10:13 +0000

    added user accessor methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many  :recipes, class_name: "Recipe", foreign_key: "user_id", dependent: :destroy
+  has_many  :reviews, class_name: "Review", foreign_key: "user_id", dependent: :destroy
 end
